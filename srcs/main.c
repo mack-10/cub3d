a0d5db6324ac68e75558a84e4d678f67fbde7fcd
Sujin Jeon <sujeon@c7r8s6.42seoul.kr>
2021-05-07 16:15:22 +0900

    argc

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: sujeon <sujeon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/18 17:37:47 by sujeon            #+#    #+#             */
-/*   Updated: 2021/05/06 19:09:27 by sujeon           ###   ########.fr       */
+/*   Updated: 2021/05/07 16:14:28 by sujeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int		main(int argc, char *argv[])
 {
 	t_main	lst;
 
-	if (argc >= 2)
+	if (argc == 2 || argc == 3)
 	{
 		check(&lst, argc, argv);
 		set_lst(&lst);
